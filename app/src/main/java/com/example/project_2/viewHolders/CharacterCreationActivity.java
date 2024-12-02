0642e4d4e249b8667708143cae14d057d0705700
SumFerus <tpruitt333@gmail.com>
2024-12-01 19:45:15 -0800

    should have added functionality to custom entry button

--- app/src/main/java/com/example/project_2/viewHolders/CharacterCreationActivity.java
+++ app/src/main/java/com/example/project_2/viewHolders/CharacterCreationActivity.java
@@ -1,26 +1,113 @@
 package com.example.project_2.viewHolders;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.example.project_2.R;
+import com.example.project_2.database.typeConverters.CharacterTrackerRepository;
+import com.example.project_2.databinding.ActivityCharacterCreationBinding;
 
 public class CharacterCreationActivity extends AppCompatActivity {
+    private static final String CHARACTER_CREATOR_USER_ID = "com.example.project_2.viewHolders.CHARACTER_CREATOR_USER_ID";
+    private ActivityCharacterCreationBinding binding;
+    private CharacterTrackerRepository repository;
+    private static final String NOT_IMPLEMENTED = "NOT IMPLEMENTED IN THIS VERSION";
+    private static final int NONE = 0;
+    private static final int CUSTOM_ENTRY = 1;
+    private static final int STANDARD_ARRAY = 2;
+    private static final int ROLL_STATS = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_character_creation);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityCharacterCreationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        repository = CharacterTrackerRepository.getRepository(getApplication());
+
+        setStatVisibility(NONE);
+
+        binding.createCharacterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Create Character Button Working!", Toast.LENGTH_SHORT).show();
+            }
         });
+
+        binding.customEntryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Custom Entry Button Working!", Toast.LENGTH_SHORT).show();
+                setStatVisibility(CUSTOM_ENTRY);
+            }
+        });
+
+        binding.standardArrayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), NOT_IMPLEMENTED, Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.rollStatsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), NOT_IMPLEMENTED, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
+    static Intent characterCreationIntentFactory(Context context, int userId) {
+        Intent intent = new Intent(context, CharacterCreationActivity.class);
+        intent.putExtra(CHARACTER_CREATOR_USER_ID, userId);
+        return intent;
+    }
+
+    private void setStatVisibility(int statEntryMethodId){
+        switch (statEntryMethodId) {
+            case NONE:
+                statTextViewVisibility(View.INVISIBLE);
+                statEditTextVisibility(View.INVISIBLE);
+
+                break;
+            case CUSTOM_ENTRY:
+                statTextViewVisibility(View.VISIBLE);
+                statEditTextVisibility(View.INVISIBLE);
+
+                break;
+            case STANDARD_ARRAY:
+                statTextViewVisibility(View.VISIBLE);
+                statEditTextVisibility(View.INVISIBLE);
+
+                break;
+            case ROLL_STATS:
+                statTextViewVisibility(View.VISIBLE);
+                statEditTextVisibility(View.INVISIBLE);
+
+                break;
+        }
+    }
+
+    private void statTextViewVisibility(int visible){
+        binding.strengthTextView.setVisibility(visible);
+        binding.dexterityTextView.setVisibility(visible);
+        binding.constitutionTextView.setVisibility(visible);
+        binding.intelligenceTextView.setVisibility(visible);
+        binding.wisdomTextView.setVisibility(visible);
+        binding.charismaTextView.setVisibility(visible);
+    }
+
+    private void statEditTextVisibility(int visible){
+        binding.strengthEditText.setVisibility(visible);
+        binding.dexterityEditText.setVisibility(visible);
+        binding.constitutionEditText.setVisibility(visible);
+        binding.intelligenceEditText.setVisibility(visible);
+        binding.wisdomEditText.setVisibility(visible);
+        binding.charismaEditText.setVisibility(visible);
+    }
+
 }
